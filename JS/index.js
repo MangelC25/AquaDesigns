5e1589ab05ace2fb9d393d1f218e3de5dd17c7b3
MangelC25 <150280388+MangelC25@users.noreply.github.com>
2024-10-05 01:13:36 -0500

    Commit
    Fix page + fox owl carousel

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,11 @@
 import setupMenu from "./Header-menu/Navbar.js"; 
 import NavResposive from "./Header-menu/NavbarToggler.js";
 
+// Solo llama a la librería si se usa un entorno que soporte CommonJS
+if (typeof require !== 'undefined') {
+    require('../node_modules/default-passive-events');
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
     setupMenu();
     NavResposive();
